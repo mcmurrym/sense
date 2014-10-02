d3c4ecb83e968d419a7961a880beed2d1fa7e44d
Matt McMurry <mattmcmurry@gmail.com>
2014-10-02 14:08:29 -0600

    restructuring, done with pods...

--- sense/LaunchBackgroundView.swift
+++ sense/LaunchBackgroundView.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class LaunchBackgroundView: UIView {
 
-    override func drawRect(rect: CGRect) {
-        var gradientLayer = CAGradientLayer()
-        
-//        var topColor = 
+    var layerAdded = false
+    
+    override func layoutSubviews() {
+        if !layerAdded {
+            
+            layerAdded = true
+            
+            var gradientLayer = CAGradientLayer()
+            
+//            var topColor = UIColor(hex: 0xF7931E)
+//            var bottomColor = UIColor(hex: 0xFF1D25)
+//            
+//            gradientLayer.colors = [topColor.CGColor, bottomColor.CGColor]
+//            gradientLayer.locations = [0.0, 1.0]
+//            
+//            var startPoint = CGPointMake(CGRectGetMidX(self.bounds), 0)
+//            var endPoint = CGPointMake(CGRectGetMidX(self.bounds), self.bounds.size.height)
+//            
+//            gradientLayer.startPoint = startPoint
+//            gradientLayer.endPoint = endPoint
+//            gradientLayer.frame = self.frame
+//            self.layer.addSublayer(gradientLayer)
+        }
     }
 }
