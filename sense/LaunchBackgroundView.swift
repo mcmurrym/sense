55cfada59d0dbca0dad3b9e8974fdf8fb32fe3c3
Matt McMurry <mattmcmurry@gmail.com>
2014-10-03 06:20:12 -0600

    enable gradient

--- sense/LaunchBackgroundView.swift
+++ sense/LaunchBackgroundView.swift
@@ -17,21 +17,25 @@ class LaunchBackgroundView: UIView {
             
             layerAdded = true
             
-            var gradientLayer = CAGradientLayer()
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = self.frame
             
-//            var topColor = UIColor(hex: 0xF7931E)
-//            var bottomColor = UIColor(hex: 0xFF1D25)
-//            
-//            gradientLayer.colors = [topColor.CGColor, bottomColor.CGColor]
-//            gradientLayer.locations = [0.0, 1.0]
-//            
-//            var startPoint = CGPointMake(CGRectGetMidX(self.bounds), 0)
-//            var endPoint = CGPointMake(CGRectGetMidX(self.bounds), self.bounds.size.height)
-//            
-//            gradientLayer.startPoint = startPoint
-//            gradientLayer.endPoint = endPoint
-//            gradientLayer.frame = self.frame
+            let topColor = UIColor(red: 247/255, green: 147/255, blue: 30/255, alpha: 1.0).CGColor
+            let bottomColor = UIColor(red: 255/255, green: 29/255, blue: 37/255, alpha: 1.0).CGColor
+            
+            let colors: [AnyObject] = [topColor, bottomColor]
+            
+            gradientLayer.colors = colors //NSArray(objects: topColor.CGColor, bottomColor.CGColor)
+            gradientLayer.locations = [0.0, 1.0]
+            
+            var startPoint = CGPointMake(CGRectGetMidX(self.bounds), 0)
+            var endPoint = CGPointMake(CGRectGetMidX(self.bounds), self.bounds.size.height)
+            
+            gradientLayer.startPoint = startPoint
+            gradientLayer.endPoint = endPoint
+
 //            self.layer.addSublayer(gradientLayer)
+            self.layer.insertSublayer(gradientLayer, atIndex: 1)
         }
     }
 }
