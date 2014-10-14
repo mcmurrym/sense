5ace15e2f1ea86541c61770dbae21e02c036abd4
Matt McMurry <mattmcmurry@gmail.com>
2014-10-14 14:14:19 -0600

    Request permission for location in place

---
+++ sense/SignUpLocationPermissionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpLocationPermissionViewController.swift
+//  sense
+//
+//  Created by Matt McMurry on 10/14/14.
+//  Copyright (c) 2014 O. C. Tanner. All rights reserved.
+//
+
+import UIKit
+
+class SignUpLocationPermissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func connectToLocation(sender: AnyObject) {
+    
+    }
+
+    @IBAction func skipLocationConnection(sender: AnyObject) {
+  
+    }
+}
